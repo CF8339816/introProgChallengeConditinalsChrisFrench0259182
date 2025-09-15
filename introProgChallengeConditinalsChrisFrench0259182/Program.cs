c5c0bbd61e44c1b00ade073a9da7c5b7a7c38b40
CF8339816 <chris_french_3dga@hotmail.com>
2025-09-15 11:09:32 -0300

    Initial commit

---
+++ introProgChallengeConditinalsChrisFrench0259182/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroProgConditionalsChrisFrench0259182
+{
+    internal class Program
+    {
+
+        enum weaponType
+        {
+            Fist = 0,
+            Pistol = 1,
+            RocketLauncher = 2,
+            SniperRifle = 3,
+        }
+
+        static int weapon = weaponType;
+        static int health = 100;
+        static string healthStatus = hStats;
+        static string Weapon = weaponName;
+        static int EnemyHealth = 100;
+        static string EnemyHealthStatus = EhStats;
+
+        static void dealdamage(int weapontype);
+
+        static
+
+
+
+            }
+
+        static void Main(string[] args)
+        {
+            HUD();
+
+            dealdamage(weaponType);
+
+            switch (EhStat)
+            {
+                case == 0:
+                    Console.WriteLine($" Health is {EnemyHealth} Enemy is Dead. ");
+                    break;
+                case <= 10 + != 0:
+            Console.WriteLine($" Health is {EnemyHealth} Enemy is in immiment danger. ");
+                    break;
+                case <= 49 + > 10:
+            Console.WriteLine($" Health is {EnemyHealth} Enemy is badly hurt. ");
+                    break;
+                case <= 74 + > 49:
+            Console.WriteLine($" Health is {EnemyHealth} Enemy is in hurt. ");
+                    break;
+                case <= 99 + > 74:
+            Console.WriteLine($" Health is {EnemyHealth} Enemy is healthy. ");
+                    break;
+                case == 100:
+                    Console.WriteLine($" Health is {EnemyHealth} Enemy is perfectly fit and healthy. ");
+                    break;
+
+
+
+
+
+
+                    If Ehealth > 0;
+
+                    {
+                        Console.WriteLine("Enemy still alive continue combat. ");
+                    }
+
+                    Else
+                {
+                        Console.WriteLine("Enemy defeated you can breath again. ");
+                    }
+
+
+            }
+            static void HUD()
+            {
+                Console.WriteLine("{0,0}{1,13}{2,9}{3,12}", "health", "weapon", "Health Status");
+                Console.WriteLine("{0,0}{1,10}{2,10}{3,10}", health, weaponName, hstat);
+
+            }
+        }
+    }
---
+++ introProgChallengeConditinalsChrisFrench0259182/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("introProgChallengeConditinalsChrisFrench0259182")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("introProgChallengeConditinalsChrisFrench0259182")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4fe08714-6f57-411c-b989-5c74762a784c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
